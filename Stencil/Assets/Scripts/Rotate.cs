8d2291cc8ac5ad0f41079c248f6902c699c5429c
Kasidit <Kasidit.ruangmunjit@gmail.com>
2021-12-03 03:42:49 +0700

    Finish

--- Stencil/Assets/Scripts/Rotate.cs
+++ Stencil/Assets/Scripts/Rotate.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using DG.Tweening;
 public class Rotate : MonoBehaviour
 {
-    public GameObject obj;
+    public Transform obj;
+    public Vector3 targetRotate;
+    public float duration;
+    public Ease ease;
     void Start()
     {
-        //obj.DoRotate(obj,)
+        obj.DOLocalRotate(targetRotate,duration,RotateMode.LocalAxisAdd).SetEase(ease).SetLoops(-1);
+        Debug.Log("Rotate!!");
     }
-
 }
