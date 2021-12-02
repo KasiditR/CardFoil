7b163588ebe86f5ff15f147a072de2b6c99c49b9
Kasidit <Kasidit.ruangmunjit@gmail.com>
2021-12-03 03:13:37 +0700

    Add Package Dotween and TMP

---
+++ Stencil/Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class Rotate : MonoBehaviour
+{
+    public GameObject obj;
+    void Start()
+    {
+        //obj.DoRotate(obj,)
+    }
+
+}
